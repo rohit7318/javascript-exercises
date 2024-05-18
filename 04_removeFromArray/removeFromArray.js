e4cff2724ddfaf6e437b23b171314559ff4d1fe1
Rohit Sharma <rorohit7318@gmail.com>
2024-05-18 09:40:45 +0530

    exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array, ...argumentsArray) {
+// // [1,2,3,4,5] , 1 ,2
+//     let newArray = [];
+//     array.forEach((elem)=>
+//     {
+//         if(!argumentsArray.includes(elem))
+//             {
+//                 newArray.push(elem);
+//             }
+//     })
+      
+//     return newArray;
+// };
 
-};
+const removeFromArray = function(array , ...args)
+{
+    /*
+    looping through the array, and returning  it's element , which are not present in a
+    args array;
+    */
+    return array.filter((element)=>{return !args.includes(element)});
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
