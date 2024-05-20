f8d173d82dd8bb90d960a2776314177d298b7892
Rohit Sharma <rorohit7318@gmail.com>
2024-05-20 22:45:59 +0530

    all functions created successfully

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) 
+{
+  if(array.length===0) return 0;
+  if(array.length===1) return array[0];
+  if(array.length===2) return array[0] + array[array.length-1];
+  return array.reduce((accumalator,current)=> accumalator+current);
+};
+
+const multiply = function(array) {
+  
+  return array.reduce((acc,elem)=>acc*=elem
+  ,1)
+};
+
+const power = function(a,b)
+{
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n)
+{
+  if(n===0) return 1;
+  let product = 1;
+  for(let i =n;i>0;i--)
+    {
+      product*=i;
+    }
+    return product;
 	
 };
 
