b6f0e852859fbec2d1805a41799757f304150624
Rohit Sharma <rorohit7318@gmail.com>
2024-05-21 22:13:02 +0530

    exercise completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,32 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayOfObjects)
+{
+    // My solution
+    // let emptyArray = [];
+    // arrayOfObjects.forEach(element => {
+    //     emptyArray.push(element.title);
+    // });
+    // return emptyArray;
+
+    return arrayOfObjects.map(book=>book.title);
 
 };
+
+
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(getTheTitles(books));
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
