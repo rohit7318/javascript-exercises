075a49ce9a2332d796abd22366f51f8c152d472a
Rohit Sharma <rorohit7318@gmail.com>
2024-05-21 16:33:37 +0530

    palindromes exercise completed

--- 09_palindromes/README.md
+++ 09_palindromes/README.md
@@ -5,7 +5,7 @@ Write a function that determines whether or not a given string is a palindrome.
 A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks:
 
 ### some palindromes:
-  - A car, a man, a maraca.
+  - A car, a man, a maraca. 
   - Rats live on no evil star.
   - Lid off a daffodil.
   - Animal loots foliated detail of stool lamina.
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,46 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    // Solution 1:
+    // let string = str.toLowerCase().split("").filter((elem)=>{
+    //     return !(elem==="." || elem==="!" ||elem===" " || elem===",")
+    // }).join("");
+
+    // let reversedString = str.toLowerCase().split("").toReversed().filter((elem)=>{
+    //     return !(elem==="." || elem==="!" ||elem===" " || elem===",")
+    // }).join("");
+
+    // if(string===reversedString) return true;
+    // else return false;
+
+    // Solution 2:
+    // Char code for numbers(1to9) 48-57 characters(atoz) 97-122
+    // let string =str.toLowerCase()
+    //             .split("")
+    //             .filter((char)=>
+    //             {
+    //                 let code = char.charCodeAt(0);
+    //                 return  (code >= 48 && code <= 57) || // numbers (0-9)
+    //                         (code >= 97 && code <= 122);  // lowercase letters (a-z)
+    //             })
+    //             .join("");
+
+    // let reversedString = string.split("").toReversed().join("");
+    // return (string === reversedString);
+
+
+    // Solution 3:
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+    const cleanedString = str
+                        .toLowerCase()
+                        .split("")
+                        .filter((char)=>alphanumerical.includes(char))
+                        .join("");
+    const reversedString = cleanedString.split("").toReversed().join("");
+    return reversedString===cleanedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
